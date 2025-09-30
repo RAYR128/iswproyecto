fc8bc2b9ea0c5ba8566423d20f69c61684a4e1e1
rayr <ningunacosa@aqui.com>
2025-09-30 19:51:08 -0300

    arreglar problema de host

--- isw/src/index.js
+++ isw/src/index.js
@@ -3,6 +3,7 @@ const express=require("express");
 const path=require("path");
 const app=express();
 const PORT=process.env.PORT||2000;
+const HOST=process.env.HOST||"127.0.0.1";
 
 app.use(express.json());
 
@@ -11,6 +12,6 @@ app.get("/api/prueba",(req,res)=>{ res.json({ok:true, msg:"funcionando!"}); });
 // rebotar lo que se envio
 app.post("/api/echo",(req,res)=>{ res.json({youSent:req.body||null}); });
 
-app.listen(PORT,()=>{
+app.listen(PORT,HOST,()=>{
 	console.log(`abierto en port ${PORT}`);
 });
